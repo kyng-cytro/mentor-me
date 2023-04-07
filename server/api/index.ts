39388dc6fe7b1533851c6f30a5a88958d6fed28b
Kyng Cytro <johndibashi2001@gamil.com>
2023-04-07 19:56:35 +0100

    :tada: init

---
+++ server/api/index.ts
@@ -0,0 +1,12 @@
+import { zh } from "h3-zod";
+import { z } from "zod";
+
+const scheme = z.object({ name: z.string().min(1, "Again Don't be lazy") });
+
+export default defineEventHandler(async (event) => {
+  await zh.useValidatedBody(event, scheme);
+
+  return {
+    status: "Ok",
+  };
+});
