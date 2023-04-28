092242d31d1cf1bfdeb9d4fe56250bacce9309c8
kyng-cytro <johndibashi2001@gmail.com>
2023-04-28 16:07:12 +0100

    completed nav-bar

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 const isDark = useDark();
 const toggleDark = useToggle(isDark);
+const navOpen = ref(false);
 </script>
 <template>
   <div
     class="font-Nunito flex h-full min-h-screen flex-col bg-white text-black dark:bg-slate-800 dark:text-white"
   >
     <!-- Nav Bar -->
-    <NavbarsLanding @click="toggleDark()" />
+    <NavbarsLanding
+      @click="toggleDark()"
+      @toggle-nav="navOpen = !navOpen"
+      :open="navOpen"
+    />
 
     <!-- Main -->
     <div class="flex-1">
